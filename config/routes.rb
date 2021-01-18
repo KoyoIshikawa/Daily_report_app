fd65fabe5554edd2f8cfa304ce338d214a754178
KoyoIshikawa <koyoi561@gmail.com>
2021-01-18 22:21:02 +0900

    デフォルトのルーティング時indexアクションを動作させるように設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
+  
+  root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
