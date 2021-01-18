b8f27d9d19105565510af6275b6202a1573a126b
KoyoIshikawa <koyoi561@gmail.com>
2021-01-19 07:39:03 +0900

    new，createアクションを追加、制約を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,18 @@
 class PostsController < ApplicationController
   def index
+    
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
   def edit
@@ -18,5 +22,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
