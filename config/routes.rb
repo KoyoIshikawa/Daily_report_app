2d80aeeed9a492406b6660219b464a9ba18d4088
KoyoIshikawa <koyoi561@gmail.com>
2021-01-18 22:19:00 +0900

    コントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/edit'
+  get 'posts/update'
+  get 'posts/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
